bfdf7c6940536458dec32e663e962dba5771915d
Victor Veytsman <vityavv@gmail.com>
2018-11-20 12:38:19 -0500

    Add a function to save the config
    thanks to the gophers slack for their help

--- save.go
+++ save.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"os"
 	"log"
+	"strings"
 	"github.com/clbanning/mxj"
 )
 
@@ -12,13 +13,9 @@ type Keybinding struct {
 }
 
 func readCombs() []Keybinding {
-	rc, err := ioutil.ReadFile(os.Getenv("HOME") + "/.config/openbox/rc.xml")
+	data, err := readRC()
 	if err != nil {
 		log.Fatal("Problem reading rc.xml: " + err.Error())
-	}
-	data, err := mxj.NewMapXml(rc)
-	if err != nil {
-		log.Fatal("Problem parsing rc.xml: " + err.Error())
 	}
 	keybindings := data[
 		"openbox_config"].(map[string]interface{})[
@@ -42,4 +39,46 @@ func readCombs() []Keybinding {
 		}
 	}
 	return commandKeybinds
+}
+
+func addComb(comb Keybinding) {
+	data, err := readRC()
+	if err != nil {
+		log.Fatal("Problem reading rc.xml: " + err.Error())
+	}
+	translated := map[string]interface{}{
+		"action": map[string]string{
+			"-name": "Execute",
+			"command": comb.command,
+		},
+		"-key": comb.key,
+	}
+	//Thanks to Kale Blankenship from the golang slack for this solution!!!!!!!!
+	var (
+		config   = data["openbox_config"].(map[string]interface{})
+		keyboard = config["keyboard"].(map[string]interface{})
+		keybind  = keyboard["keybind"].([]interface{})
+	)
+	keyboard["keybind"] = append(keybind, translated)
+	result, err := data.XmlIndent("", "	")
+	if err != nil {
+		log.Fatal("Problem re-writing XML: " + err.Error())
+	}
+	result = []byte(strings.Replace(string(result), "&", "&amp;", -1))
+	err = ioutil.WriteFile(os.Getenv("HOME") + "/.config/openbox/rc.xml", result, 0644)
+	if err != nil {
+		log.Fatal("Problem re-writing XML: " + err.Error())
+	}
+}
+
+func readRC() (mxj.Map, error) {
+	rc, err := ioutil.ReadFile(os.Getenv("HOME") + "/.config/openbox/rc.xml")
+	if err != nil {
+		return nil, err
+	}
+	data, err := mxj.NewMapXml(rc)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 }
