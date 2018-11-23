586d92a19634d18e7e7314678cf01b595ccfc71d
Victor Veytsman <vityavv@gmail.com>
2018-11-22 22:03:28 -0500

    Do lots of commenting, improve code quality, and make it a little faster

--- main.go
+++ main.go
@@ -6,11 +6,12 @@ import (
 	"github.com/gdamore/tcell"
 )
 
-var (
-	combs = readCombs()
-)
-
 func main() {
+	combs, err := readCombs()
+	if err != nil {
+		log.Fatal("Problem reading keyboard combinations: " + err.Error())
+	}
+	// Set up main variables, and set up pages
 	app := tview.NewApplication()
 	table := tview.NewTable().SetBorders(true)
 	addForm := tview.NewForm()
@@ -24,6 +25,8 @@ func main() {
 		SetTitle("OpenBox Keyboard Customizer")
 
 	//==========FORM==========
+	// Set up form and inputs
+	// I use separate inputs here because I need to be able to read from each one in the future
 	combInput := tview.NewInputField().SetLabel("Keybind")
 	combInput.SetFormAttributes(0, tcell.ColorWhite, tcell.ColorBlack, tcell.ColorWhite, tcell.ColorGreen)
 	commandInput := tview.NewInputField().SetLabel("Command")
@@ -32,8 +35,10 @@ func main() {
 		AddFormItem(combInput).
 		AddFormItem(commandInput).
 		AddButton("Add", func() {
+			// Check if a combination for that key already exists
 			for _, comb := range combs {
 				if comb.key == combInput.GetText() {
+					// Open up a modal. Doesn't draw over the existing screen, but takes it up
 					modal := tview.NewModal().
 						SetText("A combination for that key already exists!").
 						AddButtons([]string{"ok"}).
@@ -48,8 +53,12 @@ func main() {
 				key: combInput.GetText(),
 				command: commandInput.GetText(),
 			})
-			combs = readCombs()
-			addCombs(table)
+			// Update list of keyboard combinations, and then reset form and go back
+			combs = append(combs, Keybinding{
+				key: combInput.GetText(),
+				command: commandInput.GetText(),
+			})
+			addCombs(table, combs)
 			combInput.SetText("")
 			commandInput.SetText("")
 			pages.SwitchToPage("combs")
@@ -61,10 +70,12 @@ func main() {
 			app.Stop()
 		})
 
+	// Separate form for editing combinations
 	editCommandInput := tview.NewInputField().SetLabel("Command for ___")
 	editCommandInput.SetFormAttributes(0, tcell.ColorWhite, tcell.ColorBlack, tcell.ColorWhite, tcell.ColorGreen)
 	editForm.
 		AddFormItem(editCommandInput).
+		// Set add function to nil because it gets changed in the table func
 		AddButton("Edit", nil).
 		AddButton("Cancel", func() {
 			editCommandInput.SetText("")
@@ -79,36 +90,45 @@ func main() {
 	addHeader(table, 1, "Command")
 	addHeader(table, 2, "Edit")
 	addHeader(table, 3, "Delete")
-	addCombs(table)
+	addCombs(table, combs)
 	//Handle events
 	table.SetDoneFunc(func(key tcell.Key) {
+		// key == 'q' shouldn't matter here, but meh
 		if key == tcell.KeyEscape || key == 'q' {
 			app.Stop()
 		}
 	}).SetSelectedFunc(func(row, col int) {
 		switch col {
 			case 2:
+				// Column 2 is "Edit". This makes the form command-specific, with the present value in the input
 				editCommandInput.
 					SetLabel("Command for " + combs[row-1].key).
 					SetText(combs[row-1].command)
+				// Set the function to edit that *specific* combination
 				editForm.GetButton(0).SetSelectedFunc(func() {
 					editComb(combs[row-1], Keybinding{
 						key: combs[row-1].key,
 						command: editCommandInput.GetText(),
 					})
-					combs = readCombs()
-					addCombs(table)
+					// Once again reset the combinations.
+					combs[row-1] = Keybinding{
+						key: combs[row-1].key,
+						command: editCommandInput.GetText(),
+					}
+					addCombs(table, combs)
 					pages.SwitchToPage("combs")
 					editForm.GetButton(0).SetSelectedFunc(nil)
 				})
 				pages.SwitchToPage("edit")
 			case 3:
+				// Column 3 is "Delete". This opens a modal to make sure the combination wants to be deleted.
 				modal := tview.NewModal().
 					SetText("Are you sure you want to delete the combination for " + combs[row-1].key + "?").
 					AddButtons([]string{"Yes", "Cancel"}).
 					SetDoneFunc(func(buttonIndex int, buttonLabel string) {
 						if (buttonLabel == "Yes") {
 							deleteComb(combs[row-1])
+							combs = append(combs[:row-1], combs[row:]...)
 							table.RemoveRow(row)
 						}
 						app.SetRoot(pages, true).SetFocus(pages)
@@ -128,12 +148,13 @@ func main() {
 	})
 	table.Select(1, 2).SetSelectable(true, true)
 
-	err := app.SetRoot(pages, true).Run()
+	err = app.SetRoot(pages, true).Run()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+// addHeader adds a header to a table, with a yellow centered text that cannot be selected
 func addHeader(table *tview.Table, col int, name string) {
 	table.SetCell(0, col, tview.NewTableCell(name).
 		SetTextColor(tcell.ColorYellow).
@@ -142,7 +163,8 @@ func addHeader(table *tview.Table, col int, name string) {
 		SetExpansion(1))
 }
 
-func addCombs(table *tview.Table) {
+// addCombs adds the keyboard combination using the combs given
+func addCombs(table *tview.Table, combs []Keybinding) {
 	for i, comb := range combs {
 		table.SetCell(i+1, 0, tview.NewTableCell(comb.key).
 			SetSelectable(false))
--- save.go
+++ save.go
@@ -4,7 +4,6 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"log"
 	"strings"
 	"github.com/clbanning/mxj"
 )
@@ -13,10 +12,12 @@ type Keybinding struct {
 	key, command string
 }
 
-func readCombs() []Keybinding {
+// readCombs reads the key combinations openbox has set up
+// After using readRC to get the raw data in mxj.Map format, it will translate it into an array of Keybindings. It will ignore the keybindings that don't execute a command.
+func readCombs() ([]Keybinding, error) {
 	data, err := readRC()
 	if err != nil {
-		log.Fatal("Problem reading rc.xml: " + err.Error())
+		return nil, err
 	}
 	keybindings := data[
 		"openbox_config"].(map[string]interface{})[
@@ -39,13 +40,15 @@ func readCombs() []Keybinding {
 			})
 		}
 	}
-	return commandKeybinds
+	return commandKeybinds, nil
 }
 
-func addComb(comb Keybinding) {
+// addComb adds a combination to the config and reloads openbox to use it
+// After using readRC to get the raw data, it translates the keybinding given to it into a map[string]interface{} which it inserts into the data and then writes back using writeRC.
+func addComb(comb Keybinding) error {
 	data, err := readRC()
 	if err != nil {
-		log.Fatal("Problem reading rc.xml: " + err.Error())
+		return err
 	}
 	translated := map[string]interface{}{
 		"action": map[string]string{
@@ -61,47 +64,56 @@ func addComb(comb Keybinding) {
 		keybind  = keyboard["keybind"].([]interface{})
 	)
 	keyboard["keybind"] = append(keybind, translated)
-	writeRC(data)
+	return writeRC(data)
 }
+
+// deleteComb deletes a combination and reloads openbox to reflect that
+// After reading the data with readRC, it finds the combinatoin it needs, deletes it, and uses writeRC to write the data back into the file.
 //TODO: FIX THIS MESS
-func deleteComb(comb Keybinding) {
+func deleteComb(comb Keybinding) error {
 	data, err := readRC()
 	if err != nil {
-		log.Fatal("Problem reading rc.xml: " + err.Error())
+		return err
 	}
 	var (
 		config   = data["openbox_config"].(map[string]interface{})
 		keyboard = config["keyboard"].(map[string]interface{})
 		keybind  = keyboard["keybind"].([]interface{})
 	)
-	for i, inspect := range keybind {
+	for i, inspectRAW := range keybind {
+		inspect := inspectRAW.(map[string]interface{})
 		//TODO: Fix this messs
-		switch inspect.(map[string]interface{})["action"].(type) {
+		switch inspect["action"].(type) {
 			case map[string]interface{}:
-				if inspect.(map[string]interface{})["-key"].(string) == comb.key && inspect.(map[string]interface{})["action"].(map[string]interface{})["command"].(string) == comb.command {
+				action := inspect["action"].(map[string]interface{})
+				if inspect["-key"].(string) == comb.key && action["command"].(string) == comb.command {
 					keyboard["keybind"] = append(keybind[:i], keybind[i+1:]...)
 				}
 			default:
 				continue
 		}
 	}
-	writeRC(data)
+	return writeRC(data)
 }
+
+// editComb edits a keyboard combination and reloads openbox to relfect that
+// After reading the data with readRC, it changes the keyboard combination and uses writeRC to insert the edit into the file and reload openbox.
 //TODO: FIX THIS MESS
-func editComb(oldComb, comb Keybinding) {
+func editComb(oldComb, comb Keybinding) error {
 	data, err := readRC()
 	if err != nil {
-		log.Fatal("Problem reading rc.xml: " + err.Error())
+		return err
 	}
 	keybinds := data[
 		"openbox_config"].(map[string]interface{})[
 		"keyboard"].(map[string]interface{})[
 		"keybind"].([]interface{})
-	for i, inspect := range keybinds {
-		//TODO: Fix this messs
-		switch inspect.(map[string]interface{})["action"].(type) {
+	for i, inspectRAW := range keybinds {
+		inspect := inspectRAW.(map[string]interface{})
+		switch inspect["action"].(type) {
 			case map[string]interface{}:
-				if inspect.(map[string]interface{})["-key"].(string) == oldComb.key && inspect.(map[string]interface{})["action"].(map[string]interface{})["command"].(string) == oldComb.command {
+				action := inspect["action"].(map[string]interface{})
+				if inspect["-key"].(string) == oldComb.key && action["command"].(string) == oldComb.command {
 					keybinds[i] = map[string]interface{}{
 						"action": map[string]string{
 							"-name": "Execute",
@@ -114,9 +126,10 @@ func editComb(oldComb, comb Keybinding) {
 				continue
 		}
 	}
-	writeRC(data)
+	return writeRC(data)
 }
 
+// readRC returns a mxj.Map containing the data found in $HOME/.config/openbox/rc.xml
 func readRC() (mxj.Map, error) {
 	rc, err := ioutil.ReadFile(os.Getenv("HOME") + "/.config/openbox/rc.xml")
 	if err != nil {
@@ -129,18 +142,20 @@ func readRC() (mxj.Map, error) {
 	return data, nil
 }
 
-func writeRC(data mxj.Map) {
+// writeRC takes a mxj.Map, converts it to XML, and then writes it into $HOME/.config/openbox/rc.xml
+func writeRC(data mxj.Map) error {
 	result, err := data.XmlIndent("", "	")
 	if err != nil {
-		log.Fatal("Problem re-writing XML: " + err.Error())
+		return err
 	}
 	result = []byte(strings.Replace(string(result), "&", "&amp;", -1))
 	err = ioutil.WriteFile(os.Getenv("HOME") + "/.config/openbox/rc.xml", result, 0644)
 	if err != nil {
-		log.Fatal("Problem re-writing XML: " + err.Error())
+		return err
 	}
 	err = exec.Command("openbox", "--reconfigure").Run()
 	if err != nil {
-		log.Fatal("Problem reconfiguring OpenBox: " + err.Error())
+		return err
 	}
+	return nil
 }
